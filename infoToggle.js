76fbc4f06fd911ed289c9ef663bcc2453a0ea231
Kim Coder <61853524+hyoniii-web@users.noreply.github.com>
2020-03-22 13:24:54 +0900

    Delete infoToggle.js
    Inserting into HTML file.

--- infoToggle.js
+++
@@ -1,7 +0,0 @@
-const moreBtn = document.querySelector(".moreBtn");
-const title = document.querySelector(".title");
-
-moreBtn.addEventListener("click", () => {
-  moreBtn.classList.toggle("clicked");
-  title.classList.toggle("clamp");
-});
